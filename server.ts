8c6ec0369becaf2cd43e1ef4ca6631055982a345
yab <hello@factions.ai>
2024-06-10 10:32:56 -0400

    basic post is getting through

--- server.ts
+++ server.ts
@@ -4,7 +4,9 @@ const bodyParser = require('body-parser')
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
-const port = 3000
+app.use(bodyParser.json())
+
+const port = 3001
 
 const dummyUsers = [
     {
@@ -41,6 +43,8 @@ app.post('/login', (req, res) => {
 
     console.log("username", username)
     console.log("password", password)
+
+    res.send("Yes the /post endpoint says hello")
 })
 
 
